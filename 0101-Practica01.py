d41efc8d63c0af5311be96345aac3c7179312fcd
Pablo Cardozo <cardozopabloj@gmail.com>
2022-03-28 08:54:10 -0300

    Create 0101-Practica01.py
    01, 02

---
+++ 0101-Practica01.py
@@ -0,0 +1,27 @@
+
+# Ej. 1:
+
+# print(".............................")
+# print(".         ppppppppp         .")
+# print(".      pp           pp      .")
+# print(".     pp             pp     .")
+# print(".    pp    PP   PP    pp    .")
+# print(".   pp     PP   PP     pp   .")
+# print(".   pp                 pp   .")
+# print(".   pp   PP       PP   pp   .")
+# print(".    pp   PP     PP   pp    .")
+# print(".     pp    PPPPP    pp     .")
+# print(".      pp           pp      .")
+# print(".         ppppppppp         .")
+# print(".............................")
+
+# Ej. 2:
+
+from ctypes.wintypes import PINT
+
+
+print("Ingrese su nombre:", end="")
+nombre = input()
+print("Ingrese su apellido:", end="")
+apellido = input()
+print("Hola " + nombre, apellido + "!")
