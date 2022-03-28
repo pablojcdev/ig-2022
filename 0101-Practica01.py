fed513374ece247703a0c7eb48d2e684bc0ba434
Pablo Cardozo <cardozopabloj@gmail.com>
2022-03-28 13:27:21 -0300

    Update 0101-Practica01.py
    04

--- 0101-Practica01.py
+++ 0101-Practica01.py
@@ -25,11 +25,20 @@
 
 # Ej. 3:
 
-print("Ingrese lado 1: ", end="")
-lado1 = int(input())
-print("Ingrese lado 2: ", end="")
-lado2 = int(input())
-perimetro = lado1*2 + lado2*2
-print("\nPerímetro:", perimetro)
-area = lado1 * lado2
-print("Área:", area)
+# print("Ingrese lado 1: ", end="")
+# lado1 = int(input())
+# print("Ingrese lado 2: ", end="")
+# lado2 = int(input())
+# perimetro = lado1*2 + lado2*2
+# print("\nPerímetro:", perimetro)
+# area = lado1 * lado2
+# print("Área:", area)
+
+# Ej. 4:
+
+print("Ingrese numero: ", end="")
+numc = float(input())
+ent = int(numc)
+print("\nParte entera:", ent)
+frac = numc - ent
+print("Parte fraccionaria:", round(frac, 3))
