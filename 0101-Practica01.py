b0cfd75e9f373b1b0c5d7766c33262b7fbc71a15
Pablo Cardozo <cardozopabloj@gmail.com>
2022-03-28 09:38:26 -0300

    Update 0101-Practica01.py
    03

--- 0101-Practica01.py
+++ 0101-Practica01.py
@@ -17,8 +17,19 @@
 
 # Ej. 2:
 
-print("Ingrese su nombre: ", end="")
-nombre = input()
-print("Ingrese su apellido: ", end="")
-apellido = input()
-print("Hola", nombre.capitalize(), apellido.capitalize() + "!")
+# print("Ingrese su nombre: ", end="")
+# nombre = input()
+# print("Ingrese su apellido: ", end="")
+# apellido = input()
+# print("Hola", nombre.capitalize(), apellido.capitalize() + "!")
+
+# Ej. 3:
+
+print("Ingrese lado 1: ", end="")
+lado1 = int(input())
+print("Ingrese lado 2: ", end="")
+lado2 = int(input())
+perimetro = lado1*2 + lado2*2
+print("\nPerímetro:", perimetro)
+area = lado1 * lado2
+print("Área:", area)
