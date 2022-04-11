1c5ec782dbe199a8b57d8ea394d4449e09e1a59a
Pablo Cardozo <cardozopabloj@gmail.com>
2022-04-11 10:02:00 -0300

    02 test

--- 03.py
+++ 03.py
@@ -1,5 +1,7 @@
 # Ej. 1:
 
+'''
+
 num1 = int(input("Ingrese el primer número: "))
 num2 = int(input("Ingrese el segundo número: "))
 op = input ("Ingrese la operación (+, -, *, /): ")
@@ -14,3 +16,21 @@ elif op == "*":
     r = (num1 * num2)
 
 print("\n", num1, op, num2, "=", r)
+
+'''
+
+num1 = 8
+num2 = 2
+num3 = 7
+
+if num1 > num2 and num2 > num3:
+    print(num1, num2, num3)
+elif num1 > num2 and num2 > num3:
+    print(num1, num2, num3)
+# Ingrese el primer número: 8
+# Ingrese el segundo número: 2
+# Ingrese el tercer número: 7
+# Los números ordenados en forma ascendente son:
+# 2
+# 7
+# 8
