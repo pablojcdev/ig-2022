d24ddeb62a2118f538e2c5bd515ae1d26bbbd2b2
Pablo Cardozo <cardozopabloj@gmail.com>
2022-05-02 09:07:44 -0300

    04

--- 04.py
+++ 04.py
@@ -25,6 +25,8 @@ main()
 
 # Ej. 2:
 
+'''
+
 def np():
     n = int(input("Ingrese numeros' enteros positivos (finalice con 8); "))
     h = n
@@ -35,11 +37,32 @@ def np():
             h = n 
         elif n <= 1 and n != 0:
             l = n 
-    res = ("El mayor es " + str(h) + " y el menor es + str(1) + ".")
+    res = "El mayor es " + str(h) + " y el menor es " + str(l)
     return res
     # print("El mayor es", h, "y el menor es", 1)
 
 def main():
     print(np())
+
+main()
+
+'''
+
+# Ej. 4:
+
+def func(num):
+    c = 0
+    r = "No es perfecto"
+    for divisor in range(1, num):
+        if num % divisor == 0:
+            # print(divisor, "es divisor")
+            c += divisor
+    if c == num:
+        r = "Es perfecto"
+    return r
+
+def main():
+    n = int(input("Ingrese un numero entero positivo; "))
+    print(func(n))
 
 main()
