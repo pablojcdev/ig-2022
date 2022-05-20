cbe57be8fd7107f55363d71fb3f14a7e2b65788c
Pablo Cardozo <cardozopabloj@gmail.com>
2022-05-20 08:51:58 -0300

    02

--- 05.py
+++ 05.py
@@ -1,3 +1,6 @@
+# Ej. 1:
+
+'''
 def palabra(cad):
     if len(cad) >= 2:
         r = cad[-2:]*3
@@ -9,5 +12,24 @@ def palabra(cad):
 def main():
     p = input("Ingrese la palabra: ")
     print(palabra(p))
+
+main()
+'''
+
+# Ej. 2:
+
+def palabra(ext, cad):
+    r = "La función ha retornado una palabra vacía"
+    if len(ext) == 2 and len(cad)>0:
+        r = "La funcion retornó: " + ext[0] + cad + ext[-1:]
+    return r
+
+# palabra("<>", "hola")
+
+def main():
+    e = input("Ingrese los extremos: ")
+    p = input("Ingrese la palabra: ")
+
+    print(palabra(e, p))
 
 main()
