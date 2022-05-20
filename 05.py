84668b6ac9fa295f383d8a27cfb7b9cfac8a3336
Pablo Cardozo <cardozopabloj@gmail.com>
2022-05-20 09:24:40 -0300

    03

--- 05.py
+++ 05.py
@@ -18,18 +18,48 @@ main()
 
 # Ej. 2:
 
+'''
 def palabra(ext, cad):
     r = "La función ha retornado una palabra vacía"
     if len(ext) == 2 and len(cad)>0:
         r = "La funcion retornó: " + ext[0] + cad + ext[-1:]
     return r
 
-# palabra("<>", "hola")
-
 def main():
     e = input("Ingrese los extremos: ")
     p = input("Ingrese la palabra: ")
 
     print(palabra(e, p))
+
+main()
+'''
+
+# Ej. 3:
+
+def esLetra2(car):
+    res = False
+    acentos = car in "áéíóúüñÁÉÍÓÚÜÑ"
+    if ((car>='a'and car<='z') or (car>='A' and car<='Z') or acentos  ):
+        res = True
+    return res
+
+def esPalabra(pal):
+    res = True
+    i=0
+    while i<len(pal):
+        if not esLetra2(pal[i]):
+            res=False
+        i=i+1
+    return res
+
+def pal(p):
+    while len(p)%2 != 0 or not esPalabra(p):
+        p = input("Ingrese una palabra de longitud par: ")
+    r = "La función ha retornado: " + p[0: len(p)//2]
+    return r
+
+def main():
+    p = input("Ingrese una palabra de longitud par: ")
+    print(pal(p))
 
 main()
