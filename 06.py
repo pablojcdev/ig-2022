1a922c9fdc640be18dfb7779f7fc918b82a02e4c
Pablo Cardozo <cardozopabloj@gmail.com>
2022-05-27 09:54:12 -0300

    02

--- 06.py
+++ 06.py
@@ -1,3 +1,6 @@
+# Ej. 1:
+
+'''
 def estaenLista1(n, l):
     res = False
     if n in l:
@@ -33,3 +36,29 @@ def estaenLista3(n, l):
 print(estaenLista3(23, [1,2,3,4]))
 print(estaenLista3(23, [1,23,3,4]))
 print(estaenLista3(23, []))
+'''
+
+# Ej. 2:
+
+def estaenLista1(n, l):
+    res = False
+    if n in l:
+        res = True
+    return res  
+
+def cargarlista():
+    l = []
+    n = int(input("Numero: "))
+    while n != 0:
+        if estaenLista1(n, l):
+            print("Error ya esta en lista")
+            n = int(input("Numero: "))
+        if n < 0:
+            print("Error el numero debe ser positivo")
+            n = int(input("Numero: "))
+        if not estaenLista1(n, l) and n >= 1:
+            l.append(n)
+            n = int(input("Numero: "))
+    print(l)
+
+cargarlista()
